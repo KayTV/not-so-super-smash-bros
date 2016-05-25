4ffe5ac2bf3feaf095ada525bb9e7d2d4b2f24f3
Jon Hammond <jonh1016@gmail.com>
2016-05-25 15:00:04 -0600

    Removed preload from game.js and put into splash js so that all assets get loaded before game start

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -29,16 +29,7 @@ Game.prototype = {
   },
 
   preload: function() {
-  //   this.load.image('sky', 'assets/marioLevel/MarioLevelBackground.png');
-  //   this.load.image('ground', 'assets/marioLevel/ground.png');
-  //   this.load.image('star', 'assets/marioLevel/marioStar.png');
-	// 	this.load.image('box', 'assets/marioLevel/ledge2.png');
-	// 	this.load.image('littlebox', 'assets/marioLevel/box.png');
-	// 	this.load.image('pipe', 'assets/marioLevel/pipe2.png');
-	// 	this.load.image('bullet', 'assets/weapons/bullet2.png')
-  //   this.load.spritesheet('dude', 'assets/sprites/MegaManSprite2.png', 55, 55);
-  //   this.load.spritesheet('turtle', 'assets/marioLevel/marioBad.png', 45, 45);
-  //   this.load.image('move-box', 'assets/marioLevel/box.png')
+  
   },
   create: function() {
     this.add.sprite(0, 0, 'sky');
