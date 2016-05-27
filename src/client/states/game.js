2cf55a3550dfc3b872a7eb56403740e45bd5b1f7
Jon Hammond <jonh1016@gmail.com>
2016-05-27 13:24:17 -0600

    Adding multiple characters for multiple phones

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -23,9 +23,9 @@ Game.prototype = {
   init: function (playerCount) {
     this.playerCount = playerCount;
         console.log("PlayerCount:",playerCount);
-        // for (var i = 0; i <= playerCount; i++) {
-        //     inputs.push({left: true, right: true, jump: true, fire: false});
-        // }
+        for (var i = 0; i <= playerCount; i++) {
+            inputs.push({left: false, right: false, jump: false, fire: false});
+        }
 
     this.game.renderer.renderSession.roundPixels = true;
     this.game.stage.disableVisibilityChange = true;
@@ -45,10 +45,7 @@ Game.prototype = {
     //  We will enable physics for any object that is created in this group
     this.platforms.enableBody = true;
 
-    // Here we create the ground.
     var ground = this.platforms.create(0, this.world.height - 64, 'ground');
-
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
 
     var ledge = this.platforms.create(250, 350, 'box');
@@ -67,24 +64,33 @@ Game.prototype = {
     ledge.body.immovable = true;
 
     // Player1/Player and settings
-    this.player1 = this.add.sprite(32, this.world.height - 150, 'megaman');
+    // this.player1 = this.add.sprite(32, this.world.height - 150, 'megaman');
+
+    // Phone Characters
+    socket.on('game-update', function(data) {
+      inputs[data.player] = data;
+    });
+    for (var i = 0; i<this.playerCount; i++) {
+      players.push(new Character(i))
+    }
+
     this.player2 = this.add.sprite(200, this.world.height - 150, 'kirby');
     this.player2.scale.setTo(1.5,1.5);
     this.player3 = this.add.sprite(300, this.world.height -150, 'pikachu');
 
     //  We need to enable physics on the player1
-		this.player1.anchor.set(0.5);
+		// this.player1.anchor.set(0.5);
     this.player2.anchor.set(2);
     this.player3.anchor.set(0.5);
 
-    this.physics.enable(this.player1, Phaser.Physics.ARCADE);
+    // this.physics.enable(this.player1, Phaser.Physics.ARCADE);
     this.physics.enable(this.player2, Phaser.Physics.ARCADE);
     this.physics.enable(this.player3, Phaser.Physics.ARCADE);
 
     //  Player physics properties. Give the little guy a slight bounce.
-    this.player1.body.bounce.y = 0.2;
-    this.player1.body.gravity.y = 300;
-    this.player1.body.collideWorldBounds = true;
+    // this.player1.body.bounce.y = 0.2;
+    // this.player1.body.gravity.y = 300;
+    // this.player1.body.collideWorldBounds = true;
 
     this.player2.body.bounce.y = 0.2;
     this.player2.body.gravity.y = 300;
@@ -94,11 +100,10 @@ Game.prototype = {
     this.player3.body.gravity.y = 300;
     this.player3.body.collideWorldBounds = true;
 
-    this.player1.animations.add('left', [0, 1, 2, 3], 13, true);
-    this.player1.animations.add('right', [6, 7, 8, 9], 13, true);
-		this.player1.animations.add('jump', [10], 13, true);
-		this.player1.animations.add('jumpdown', [11], 13, true);
-    this.player1.animations.add('hit', [12], 13, true);
+    // this.player1.animations.add('left', [0, 1, 2, 3], 13, true);
+    // this.player1.animations.add('right', [6, 7, 8, 9], 13, true);
+		// this.player1.animations.add('jump', [10], 13, true);
+    // this.player1.animations.add('hit', [12], 13, true);
 
     this.player2.animations.add('left', [20, 19, 18, 17, 16, 15, 14, 13, 12, 11], 13, true);
     this.player2.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 13, true);
@@ -130,60 +135,59 @@ Game.prototype = {
   },
   update: function() {
     //  Collide the player with platforms
-    this.physics.arcade.collide(this.player1, this.platforms);
-    this.physics.arcade.collide(this.player1, this.moveBox, this.customSep, null, this);
+    // this.physics.arcade.collide(this.player1, this.platforms);
+    //
 
     this.physics.arcade.collide(this.player2, this.platforms);
-    this.physics.arcade.collide(this.player2, this.moveBox, this.customSep, null, this);
 
     this.physics.arcade.collide(this.player3, this.platforms);
     this.physics.arcade.collide(this.player3, this.moveBox, this.customSep, null, this);
 
-    var standing = this.player1.body.blocked.down || this.player1.body.touching.down;
+    // var standing = this.player1.body.blocked.down || this.player1.body.touching.down;
+    //
+    // //  Player1 Movement
+    // this.player1.body.velocity.x = 0;
 
-    //  Player1 Movement
-    this.player1.body.velocity.x = 0;
+    // var self = this;
+    // socket.on('game-update', function(data) {
+    //   self.right = data.right;
+    //   self.left = data.left;
+    //   self.jump = data.jump;
+    //   self.fire = data.fire;
+    // })
 
-    var self = this;
-    socket.on('game-update', function(data) {
-      self.right = data.right;
-      self.left = data.left;
-      self.jump = data.jump;
-      self.fire = data.fire;
-    })
-
-    if (this.left === true)
-    {
-        //  Move to the left
-        this.player1.body.velocity.x = -150;
-        this.player1.animations.play('left');
-    }
-    else if (this.right === true)
-    {
-        //  Move to the right
-        // console.log("right");
-        this.player1.body.velocity.x = 150;
-        this.player1.animations.play('right');
-    }
-		else if(this.jump === true)
-		{
-			this.player1.animations.play('jump');
-		}
-    else
-    {
-      //  Stand still
-      this.player1.animations.stop();
-      this.player1.frame = 5;
-    }
-    if(this.fire === true)
-		{
-			this.fireGun();
-		}
-    //  Allow the player1 to jump if they are touching the ground.
-    if (this.jump === true && this.player1.body.touching.down)
-    {
-        this.player1.body.velocity.y = -350;
-    }
+    // if (this.left === true)
+    // {
+    //     //  Move to the left
+    //     this.player1.body.velocity.x = -150;
+    //     this.player1.animations.play('left');
+    // }
+    // else if (this.right === true)
+    // {
+    //     //  Move to the right
+    //     // console.log("right");
+    //     this.player1.body.velocity.x = 150;
+    //     this.player1.animations.play('right');
+    // }
+		// else if(this.jump === true)
+		// {
+		// 	this.player1.animations.play('jump');
+		// }
+    // else
+    // {
+    //   //  Stand still
+    //   this.player1.animations.stop();
+    //   this.player1.frame = 5;
+    // }
+    // if(this.fire === true)
+		// {
+		// 	this.fireGun();
+		// }
+    // //  Allow the player1 to jump if they are touching the ground.
+    // if (this.jump === true && this.player1.body.touching.down)
+    // {
+    //     this.player1.body.velocity.y = -350;
+    // }
 
     // Player2 Movement
     this.player2.body.velocity.x = 0;
@@ -237,24 +241,24 @@ Game.prototype = {
     }
 
   },
-  fireGun: function() {
-    if (this.time.now > this.nextFire && this.bullets.countDead() > 0)
-    {
-        this.nextFire = this.time.now + this.fireRate;
-
-        var bullet = this.bullets.getFirstDead();
-
-        bullet.reset(this.player1.x, this.player1.y);
-        if(this.right === true) {
-          bullet.body.velocity.x = 400;
-        }
-        if(this.left === true) {
-          bullet.body.velocity.x = -400;
-        }
-        else {
-          bullet.body.velocity.x = 400;
-        }
-        // this.physics.arcade.moveToXY(bullet, 500, 500, 400);
-    }
-  },
+  // fireGun: function() {
+  //   if (this.time.now > this.nextFire && this.bullets.countDead() > 0)
+  //   {
+  //       this.nextFire = this.time.now + this.fireRate;
+  //
+  //       var bullet = this.bullets.getFirstDead();
+  //
+  //       bullet.reset(this.player1.x, this.player1.y);
+  //       if(this.right === true) {
+  //         bullet.body.velocity.x = 400;
+  //       }
+  //       if(this.left === true) {
+  //         bullet.body.velocity.x = -400;
+  //       }
+  //       else {
+  //         bullet.body.velocity.x = 400;
+  //       }
+  //       // this.physics.arcade.moveToXY(bullet, 500, 500, 400);
+  //   }
+  // },
 }
