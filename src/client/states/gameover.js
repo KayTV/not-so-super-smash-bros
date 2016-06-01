f2eee9e7d3083b90961166c7423ea9aeb84152b2
Jon Hammond <jonh1016@gmail.com>
2016-06-01 14:57:47 -0600

    Added basic structure to gameoverJs

---
+++ src/client/states/gameover.js
@@ -0,0 +1,60 @@
+function GameOver () {};
+
+GameOver.prototype = {
+  init: function(winner, playerCount) {
+    this.winner = winner;
+    this.playerCount = playerCount;
+
+    this.titleText = game.make.text(game.world.centerX, 100, 'Not-So-Super Smash Bros.', {
+      font: '40px Mario',
+      align: 'center',
+      fill: 'red'
+    });
+
+    this.winnerText = game.make.text(game.world.centerX, 300, 'The winner is: (insert player number or sprite name here)', {
+      font: '40px Mario',
+      align: 'center',
+      fill: 'red'
+    });
+  },
+  create: function () {
+    game.add.sprite(0, 0, 'menu');
+    game.add.existing(this.titleText);
+    game.add.existing(this.winnerText);
+    // Make sprite dynamic
+    this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'kirby' + this.winner);
+    this.sprite.scale.setTo(3.0);
+    this.restartGame();
+  },
+  restartGame: function () {
+    var text = game.add.text(game.world.centerX, game.world.height * 0.8, 'Rematch?', {
+      font: '40px Mario',
+      align: 'center',
+      fill: 'red'
+    });
+    var hoverTrue = function (button) {
+      button.fill = "#30DEF8";
+      button.stroke = "rgba(200, 200, 200, 0.5)";
+    };
+    var hoverFalse = function (button) {
+      button.fill = 'red';
+      button.stroke = "rgba(0,0,0,0)";
+    };
+    var onClick = function () {
+      game.state.start('Game', true, false, this.playerCount);
+    }.bind(this);
+
+    text.stroke = "rgba(0,0,0,0)";
+    text.strokeThickness = 4;
+    text.inputEnabled = true;
+    text.events.onInputUp.add(onClick);
+    text.events.onInputOver.add(hoverTrue);
+    text.events.onInputOut.add(hoverFalse);
+  },
+  preload: function () {
+
+  },
+  update: function() {
+
+  }
+};
