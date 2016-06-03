fc5c13111e315136b919a133a60234e3db5d95fd
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-06-03 16:28:33 -0600

    fixed socket issue, no more than 5 players!

--- src/server/app.js
+++ src/server/app.js
@@ -54,7 +54,7 @@ io.on('connect', function(socket){
       console.log("Joined",data.gameRoom)
       socket.room = data.gameRoom;
 
-      if (rooms[data.gameRoom].players < 5) {
+      if (rooms[data.gameRoom].players) {
         rooms[data.gameRoom].players++;
         console.log("Players:",rooms[data.gameRoom].players)
       } else {
@@ -62,11 +62,12 @@ io.on('connect', function(socket){
         console.log("Players:",rooms[data.gameRoom].players)
       }
       console.log("Phone:", data.gameRoom);
-      io.sockets.in(rooms[socket.room].id).emit('player-joined', rooms[data.gameRoom].players);
+      if(rooms[data.gameRoom].players < 5) {
+        io.sockets.in(rooms[socket.room].id).emit('player-joined', rooms[data.gameRoom].players);
 
-      var playerId = rooms[data.gameRoom].players - 1;
-      socket.emit('success-join', playerId);
-
+        var playerId = rooms[data.gameRoom].players - 1;
+        socket.emit('success-join', playerId);
+      }
       if (rooms[data.gameRoom].players >= 1 && rooms[data.gameRoom].players < 5 ) {
         io.sockets.in(rooms[socket.room].id).emit('start-game');
       }
